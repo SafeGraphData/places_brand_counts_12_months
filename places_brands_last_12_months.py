5550a941e1c34805f797ac5306ce6e38bb75fa7e
sgclarissa <148800809+safegraph-clarissa@users.noreply.github.com>
2026-08-20 12:32:53 -0500

    Keep-alive commit - 2026-08-20 17:32:52.816234

--- places_brands_last_12_months.py
+++ places_brands_last_12_months.py
@@ -1263,3 +1263,4 @@ st.markdown(css, unsafe_allow_html=True)
 # Keep-alive comment: 2026-08-19 11:31:50.342234
 # Keep-alive comment: 2026-08-19 21:31:49.247239
 # Keep-alive comment: 2026-08-20 07:31:51.181425
+# Keep-alive comment: 2026-08-20 17:32:51.752062
